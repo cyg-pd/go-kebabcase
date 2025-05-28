058598406322bbd3b32f27cead4258db607ae956
PID-阿貴 <ahkui_liew@mail.chungyo.net>
2025-05-28 16:30:59 +0800

    test: b.Loop() only support go1.24
    Signed-off-by: PID-阿貴 <ahkui_liew@mail.chungyo.net>

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -7,7 +7,7 @@ import (
 func BenchmarkUnchangedLong(b *testing.B) {
 	var s = "invite_your_customers_add_invites"
 	b.SetBytes(int64(len(s)))
-	for b.Loop() {
+	for n := 0; n < b.N; n++ {
 		Kebabcase(s)
 	}
 }
@@ -15,7 +15,7 @@ func BenchmarkUnchangedLong(b *testing.B) {
 func BenchmarkUnchangedSimple(b *testing.B) {
 	var s = "sample_text"
 	b.SetBytes(int64(len(s)))
-	for b.Loop() {
+	for n := 0; n < b.N; n++ {
 		Kebabcase(s)
 	}
 }
@@ -23,14 +23,14 @@ func BenchmarkUnchangedSimple(b *testing.B) {
 func BenchmarkModifiedUnicode(b *testing.B) {
 	var s = "ß_ƒ_foo"
 	b.SetBytes(int64(len(s)))
-	for b.Loop() {
+	for n := 0; n < b.N; n++ {
 		Kebabcase(s)
 	}
 }
 func BenchmarkModifiedLong(b *testing.B) {
 	var s = "inviteYourCustomersAddInvites"
 	b.SetBytes(int64(len(s)))
-	for b.Loop() {
+	for n := 0; n < b.N; n++ {
 		Kebabcase(s)
 	}
 }
@@ -38,7 +38,7 @@ func BenchmarkModifiedLong(b *testing.B) {
 func BenchmarkModifiedLongSpecialChars(b *testing.B) {
 	var s = "FOO:BAR$BAZ__Sample    Text___"
 	b.SetBytes(int64(len(s)))
-	for b.Loop() {
+	for n := 0; n < b.N; n++ {
 		Kebabcase(s)
 	}
 }
@@ -46,7 +46,7 @@ func BenchmarkModifiedLongSpecialChars(b *testing.B) {
 func BenchmarkModifiedSimple(b *testing.B) {
 	var s = "sample text"
 	b.SetBytes(int64(len(s)))
-	for b.Loop() {
+	for n := 0; n < b.N; n++ {
 		Kebabcase("sample text")
 	}
 }
@@ -54,7 +54,7 @@ func BenchmarkModifiedSimple(b *testing.B) {
 func BenchmarkModifiedUnicode2(b *testing.B) {
 	var s = "ẞ•¶§ƒ˚foo˙∆˚¬"
 	b.SetBytes(int64(len(s)))
-	for b.Loop() {
+	for n := 0; n < b.N; n++ {
 		Kebabcase(s)
 	}
 }
@@ -62,7 +62,7 @@ func BenchmarkModifiedUnicode2(b *testing.B) {
 func BenchmarkLeadingUnderscoresDigitUpper(b *testing.B) {
 	var s = "_5TEst"
 	b.SetBytes(int64(len(s)))
-	for b.Loop() {
+	for n := 0; n < b.N; n++ {
 		Kebabcase(s)
 	}
 }
@@ -70,7 +70,7 @@ func BenchmarkLeadingUnderscoresDigitUpper(b *testing.B) {
 func BenchmarkDigitUpper(b *testing.B) {
 	var s = "5TEst"
 	b.SetBytes(int64(len(s)))
-	for b.Loop() {
+	for n := 0; n < b.N; n++ {
 		Kebabcase(s)
 	}
 }
@@ -78,7 +78,7 @@ func BenchmarkDigitUpper(b *testing.B) {
 func BenchmarkDigitUpper2(b *testing.B) {
 	var s = "lk0B@bFmjrLQ_Z6YL"
 	b.SetBytes(int64(len(s)))
-	for b.Loop() {
+	for n := 0; n < b.N; n++ {
 		Kebabcase(s)
 	}
 }
